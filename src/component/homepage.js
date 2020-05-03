5062d25f3d3825ce5dad2e4e86fdcdd20504db28
tarotiger <kenxm.el@gmail.com>
2020-05-03 19:45:27 +1000

    Added functionality to switch between tabs

--- src/component/homepage.js
+++ src/component/homepage.js
@@ -2,9 +2,45 @@ import React from "react";
 import NavBar from "./navbar.js"
 
 export default class HomePage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            home: true,
+            blog: false,
+            contact: false 
+        }
+    }
+
+    switchTabs(tab) {
+        let tabs = {
+            home: false,
+            blog: false,
+            contact: false
+        }
+        tabs[tab] = true; 
+
+        console.log(tabs);
+        console.log("Tabs is", tab);
+
+        this.setState(tabs);
+    }
+
     render() {
+        // TODO: Test case 
+        let myText;
+        if (this.state.home === true) {
+            myText = "I am currently at home";
+        } else if (this.state.blog === true) {
+            myText = "I am currently at the blog";
+        } else {
+            myText = "I am currently at contact"
+        }
+        
         return (
-            <NavBar/>
+            <React.Fragment>
+                <NavBar handleClick={this.switchTabs.bind(this)}/>
+                <div>{myText}</div>
+            </React.Fragment>  
         )
     }
 }
