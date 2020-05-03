01fd0f012d5675b9322e92056d19747d2e42f98e
tarotiger <kenxm.el@gmail.com>
2020-05-03 19:46:49 +1000

    In progress title

---
+++ src/component/title.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./title.css";
+
+export default class Title extends React.Component {
+    render() {
+        return(
+            <div className="title">
+                <p>Kenneth Lu</p>
+            </div>
+        );
+    }
+}
