2f3b2ee11cf36b2f91eb67861f53829d52585e66
tarotiger <kenxm.el@gmail.com>
2020-05-05 18:14:37 +1000

    Changed folder structure

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom'; 
-import HomePage from './component/homepage.js';
-import "./index.css";
-
-ReactDOM.render(
-    <HomePage/>,
-    document.getElementById('root')
-)
