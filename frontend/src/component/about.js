ed7964100f3dce0efeb546d8f1647f40f17ad755
Kenneth Lu <z5259931@cse.unsw.edu.au>
2020-05-30 13:33:00 +1000

    Finished about page

--- frontend/src/component/about.js
+++ frontend/src/component/about.js
@@ -1,24 +1,49 @@
 import React from "react";
+import "typeface-roboto";
 import "./about.css";
+import { Typography } from "@material-ui/core";
 
 export default class About extends React.Component {
     render() {
         return(
             <React.Fragment>
-                <p className="about-text">
-                    Hey there, I'm studying Computer Science at the University of New South Wales. This is me 
-                    below:
-                </p>
-                <img alt="about-me" className="about-img" src={require("../assets/about.PNG")}></img>
-                <p className="about-text">
-                    My hobbies are all over the place, I enjoy a good dose of video games especially those 
-                    of the RPG genre as well as boardgames. Coronavirus has forced me to pick up running to 
-                    maintain some semblence of fitness.
-                    Currently at UNSW, I am a Workshop Developer of CSESoc Compclub Subcommittee which organises 
-                    and creates workshops for high school students who are interested in learning about coding. 
-                    It has been a rewarding experience being able to give back what I have learnt and seeing 
-                    others also share the same passion as me. 
-                </p> 
+                <img alt="about-me" className="about-img" src={require("../assets/placeholder.jpg")}></img>
+                <Typography
+                    variant="h5">
+                    Introduction
+                </Typography>
+                <br></br>
+                <Typography 
+                    className="about-text"
+                    variant="body1">
+                    Hi, I 'm Kenneth Lu. I am currently completing my education in Computer Science at the University of New South Wales.
+                </Typography>
+                <br></br>
+                <Typography
+                    variant="h5">
+                        What I enjoy
+                </Typography>
+                <br></br>
+                <Typography 
+                    className="about-text"
+                    variant="body1">
+                    I am passionate about building practical software and I mainly focus on the frontend. This site was built using ReactJS and with the help of libraries such as <i>material-ui</i> and <i>react-transition-group</i>.
+                </Typography>
+                <br></br>
+                <Typography
+                    variant="h5">
+                        Skills
+                </Typography>
+                <br></br>
+                <Typography 
+                    className="about-text"
+                    variant="body1">
+                    I have worked with and have experience in these languages:
+                </Typography>
+                <Typography
+                    variant="button">
+                    [HTML, CSS, JS] [ReactJS] [C] [Java] [SQL]
+                </Typography> 
             </React.Fragment>
         );
     }
