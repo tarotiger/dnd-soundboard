67a7cd841789bcda78f0870fa9086b85994ba098
Kenneth Lu <z5259931@cse.unsw.edu.au>
2020-06-01 02:15:53 +1000

    Added additional contact options

--- src/component/contact.js
+++ src/component/contact.js
@@ -1,5 +1,7 @@
 import React from "react";
 import axios from "axios";
+import "typeface-roboto";
+import { Typography } from "@material-ui/core";
 import { TextField, Button, Snackbar } from "@material-ui/core";
 import { Alert } from '@material-ui/lab';
 import "./contact.css";
@@ -39,6 +41,7 @@ export default class Contact extends React.Component {
 
     handleSubmit(event) {
         event.preventDefault();
+        this.resetForm();
 
         this.setState({
             sending: true
@@ -56,7 +59,6 @@ export default class Contact extends React.Component {
                     sent: true,
                     status: 'Message sent successfully!'
                 })
-                this.resetForm();
             } else {
                 this.setState({
                     error: true,
@@ -80,69 +82,78 @@ export default class Contact extends React.Component {
         this.setState({
             name: '',
             email: '',
-            message: '',
-            sending: false
+            message: ''
         })
     }
 
     render() {
         return(
             <React.Fragment>
-                { this.state.sending ? (
-                    <form className="contact-form">
-                        <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
-                    </form>
-                ) : (
-                    <React.Fragment>
-                        <form className="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
-                            <TextField 
-                                className="form-textfield"
-                                fullWidth
-                                id="filled-basic"
-                                label="Name" 
-                                onChange={this.onNameChange.bind(this)}
-                                variant="outlined"/>
-                            <TextField 
-                                className="form-textfield"
-                                fullWidth
-                                id="filled-basic" 
-                                label="Email" 
-                                onChange={this.onMailChange.bind(this)}
-                                type="email"
-                                variant="outlined" />
-                            <TextField 
-                                className="form-textfield"
-                                fullWidth
-                                id="filled-basic"
-                                label="Message" 
-                                multiline
-                                onChange={this.onMessageChange.bind(this)}
-                                rows="4"
-                                variant="outlined" />
-                            <Button
-                                color="primary"
-                                size="large"
-                                type="submit">
-                                    Submit
-                            </Button>
-                        </form>
-                        <Snackbar 
-                            open={this.state.sent} 
+                <form className="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
+                    <Typography>
+                        Feel free to email me at kenlu.me08@gmail.com 
+                    </Typography>
+                    <br></br>
+                    <Typography
+                        variant="h5">
+                        OR
+                    </Typography>
+                    <TextField 
+                        className="form-textfield"
+                        fullWidth
+                        id="filled-basic"
+                        label="Name" 
+                        onChange={this.onNameChange.bind(this)}
+                        value={this.state.name}
+                        variant="outlined"/>
+                    <TextField 
+                        className="form-textfield"
+                        fullWidth
+                        id="filled-basic" 
+                        label="Email" 
+                        onChange={this.onMailChange.bind(this)}
+                        type="email"
+                        value={this.state.email}
+                        variant="outlined" />
+                    <TextField 
+                        className="form-textfield"
+                        fullWidth
+                        id="filled-basic"
+                        label="Message" 
+                        multiline
+                        onChange={this.onMessageChange.bind(this)}
+                        rows="4"
+                        value={this.state.message}
+                        variant="outlined" />
+                    <Button
+                        color="primary"
+                        size="large"
+                        type="submit">
+                            Submit
+                    </Button>
+                </form>
+                <Snackbar
+                    open={this.state.sending}
+                    autoHideDuration={5000}>
+                    <Alert severity="info">
+                        Sending...
+                    </Alert>
+                </Snackbar>
+                <Snackbar 
+                    open={this.state.sent} 
+                    autoHideDuration={5000}>
+                    { this.state.error ? (
+                        <Alert severity="error"
                             autoHideDuration={5000}>
-                            { this.state.error ? (
-                                <Alert severity="error"
-                                    autoHideDuration={5000}>
-                                    {this.state.status}
-                                </Alert>
-                            ) : (
-                                <Alert severity="success" 
-                                    autoHideDuration={5000}>
-                                    Mail successfully sent!
-                                </Alert>
-                            )}      
-                        </Snackbar>
-                    </React.Fragment>
-                )}
+                            {this.state.status}
+                        </Alert>
+                    ) : (
+                        <Alert severity="success" 
+                            autoHideDuration={5000}>
+                            Mail successfully sent!
+                        </Alert>
+                    )}      
+                </Snackbar>
             </React.Fragment>
         );
     }
