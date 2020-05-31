676aaacb22a62921575153ff705665c8218a5505
Kenneth Lu <z5259931@cse.unsw.edu.au>
2020-05-31 21:25:35 +1000

    Updates

---
+++ precache-manifest.11098cfac4f0ab1501876590038ba52b.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "860da607708e00bc2ecbc6a79059aa5c",
+    "url": "/dnd-soundboard/index.html"
+  },
+  {
+    "revision": "625c9dccd2283aa15813",
+    "url": "/dnd-soundboard/static/css/2.c88bcea4.chunk.css"
+  },
+  {
+    "revision": "5b5044adcd4597d64e25",
+    "url": "/dnd-soundboard/static/css/main.2dbd1288.chunk.css"
+  },
+  {
+    "revision": "625c9dccd2283aa15813",
+    "url": "/dnd-soundboard/static/js/2.7f753d64.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/dnd-soundboard/static/js/2.7f753d64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b5044adcd4597d64e25",
+    "url": "/dnd-soundboard/static/js/main.afa8d48d.chunk.js"
+  },
+  {
+    "revision": "bf5e175adc867e060e65",
+    "url": "/dnd-soundboard/static/js/runtime-main.5d50adb6.js"
+  },
+  {
+    "revision": "70d406db01b5a6791672334bd4cce16b",
+    "url": "/dnd-soundboard/static/media/Boss.70d406db.png"
+  },
+  {
+    "revision": "b6009bc3b89f4173d6d32a8c6c2d319d",
+    "url": "/dnd-soundboard/static/media/Boss.b6009bc3.mp3"
+  },
+  {
+    "revision": "15279c2b8c2922f217f3991ea02f54a4",
+    "url": "/dnd-soundboard/static/media/Carriage.15279c2b.mp3"
+  },
+  {
+    "revision": "b21c4a41e88b5a15ccb0957da25d465b",
+    "url": "/dnd-soundboard/static/media/Carriage.b21c4a41.png"
+  },
+  {
+    "revision": "12150fcdaf401c8da77d98ecdff0c7e0",
+    "url": "/dnd-soundboard/static/media/Cave.12150fcd.png"
+  },
+  {
+    "revision": "2fae2ac91d1b17130ae6af177d329c80",
+    "url": "/dnd-soundboard/static/media/Cave.2fae2ac9.mp3"
+  },
+  {
+    "revision": "083598b7ef54dc697325ead8554f769a",
+    "url": "/dnd-soundboard/static/media/Cheer.083598b7.mp3"
+  },
+  {
+    "revision": "c8bed3c57628059f20c8b92056a952f0",
+    "url": "/dnd-soundboard/static/media/Cheer.c8bed3c5.png"
+  },
+  {
+    "revision": "9b60b16fdc243b254798d478dde6b66c",
+    "url": "/dnd-soundboard/static/media/Combat (Plains).9b60b16f.png"
+  },
+  {
+    "revision": "e12492b720413da2f8b34873f7883bac",
+    "url": "/dnd-soundboard/static/media/Combat (Plains).e12492b7.mp3"
+  },
+  {
+    "revision": "09695515eab83864b7185d65daf6a8d9",
+    "url": "/dnd-soundboard/static/media/Explosion.09695515.png"
+  },
+  {
+    "revision": "89290e8f58008faaa9d8b14db3f65e5c",
+    "url": "/dnd-soundboard/static/media/Explosion.89290e8f.mp3"
+  },
+  {
+    "revision": "29888a124ebc3b9b871df5f3ae578732",
+    "url": "/dnd-soundboard/static/media/Festival.29888a12.mp3"
+  },
+  {
+    "revision": "d676f4b37c196ed945eb3faa833f546d",
+    "url": "/dnd-soundboard/static/media/Festival.d676f4b3.png"
+  },
+  {
+    "revision": "6204a6feb4acc3353c9c6a5a6fbff1fc",
+    "url": "/dnd-soundboard/static/media/Fire.6204a6fe.png"
+  },
+  {
+    "revision": "ad13b0c142627969e21e7703992a3194",
+    "url": "/dnd-soundboard/static/media/Fire.ad13b0c1.mp3"
+  },
+  {
+    "revision": "888b57482656c4c1aee1504f46d35879",
+    "url": "/dnd-soundboard/static/media/Goblin Cave.888b5748.png"
+  },
+  {
+    "revision": "f9d7a7906ef4aacf19dc9ade3a62ddba",
+    "url": "/dnd-soundboard/static/media/Goblin Cave.f9d7a790.mp3"
+  },
+  {
+    "revision": "613f49a02fa7cf54e27006b1fc917372",
+    "url": "/dnd-soundboard/static/media/Haunted.613f49a0.png"
+  },
+  {
+    "revision": "9fa99c0c31e2a75dee50568f1e029145",
+    "url": "/dnd-soundboard/static/media/Haunted.9fa99c0c.mp3"
+  },
+  {
+    "revision": "080c7c4181ad47fef8022fe4dae4b20a",
+    "url": "/dnd-soundboard/static/media/Lost Mine.080c7c41.mp3"
+  },
+  {
+    "revision": "cc0c49fcc522cdec43245b743dc4f2e0",
+    "url": "/dnd-soundboard/static/media/Lost Mine.cc0c49fc.png"
+  },
+  {
+    "revision": "29a32d96d3acd765018105f8336cdc3d",
+    "url": "/dnd-soundboard/static/media/Medieval Town.29a32d96.png"
+  },
+  {
+    "revision": "f2ad9e20c248e817bc77da32cee0f96a",
+    "url": "/dnd-soundboard/static/media/Medieval Town.f2ad9e20.mp3"
+  },
+  {
+    "revision": "21d2069868556e46421c810a7bc495e0",
+    "url": "/dnd-soundboard/static/media/Megumin.21d20698.png"
+  },
+  {
+    "revision": "67d4f81188c8fb241db451ef318a1b76",
+    "url": "/dnd-soundboard/static/media/Megumin.67d4f811.mp3"
+  },
+  {
+    "revision": "40ce903c2110d50585c8bca95cde1df7",
+    "url": "/dnd-soundboard/static/media/Night.40ce903c.png"
+  },
+  {
+    "revision": "499eef09129667229006b4aa2bba9ae8",
+    "url": "/dnd-soundboard/static/media/Night.499eef09.mp3"
+  },
+  {
+    "revision": "8d960ed949c5b99612771be8ba11dce7",
+    "url": "/dnd-soundboard/static/media/Rain.8d960ed9.png"
+  },
+  {
+    "revision": "93f847dfa527637adb651b3f761b47bd",
+    "url": "/dnd-soundboard/static/media/Rain.93f847df.mp3"
+  },
+  {
+    "revision": "2686cc854e306da390f8f0d1afb65025",
+    "url": "/dnd-soundboard/static/media/Sword.2686cc85.mp3"
+  },
+  {
+    "revision": "cadb2b4b93dfdfd751b11e67703366c6",
+    "url": "/dnd-soundboard/static/media/Sword.cadb2b4b.png"
+  },
+  {
+    "revision": "77a3ad1a64036e76dc2e330fb99b2f1b",
+    "url": "/dnd-soundboard/static/media/Tavern Music.77a3ad1a.png"
+  },
+  {
+    "revision": "c00757570c8c713a37ba38d6f09ab2ce",
+    "url": "/dnd-soundboard/static/media/Tavern Music.c0075757.mp3"
+  },
+  {
+    "revision": "4a3ccee38e9c8caad28a44eee894350a",
+    "url": "/dnd-soundboard/static/media/Tavern.4a3ccee3.mp3"
+  },
+  {
+    "revision": "f3e1c49121804164460e3dcf5d01cbc6",
+    "url": "/dnd-soundboard/static/media/Tavern.f3e1c491.png"
+  },
+  {
+    "revision": "a53591c1bddbdae77f1b0943717beae1",
+    "url": "/dnd-soundboard/static/media/Thunder.a53591c1.mp3"
+  },
+  {
+    "revision": "ae6c2b35aebd5cc55e735fc6101b13ab",
+    "url": "/dnd-soundboard/static/media/Thunder.ae6c2b35.png"
+  },
+  {
+    "revision": "47a99975ae7f4e2c29e7529e9e71ee0e",
+    "url": "/dnd-soundboard/static/media/Woodland Campsite.47a99975.mp3"
+  },
+  {
+    "revision": "54efba57174ac311d642093f9c00e5f5",
+    "url": "/dnd-soundboard/static/media/Woodland Campsite.54efba57.png"
+  },
+  {
+    "revision": "53930235f9e87f8e798b20445ffa6849",
+    "url": "/dnd-soundboard/static/media/close.53930235.png"
+  },
+  {
+    "revision": "fb908401e411db55f6d92b54d83cef7e",
+    "url": "/dnd-soundboard/static/media/logo.fb908401.png"
+  },
+  {
+    "revision": "0832cabb53798a95537cb84808ee3e5c",
+    "url": "/dnd-soundboard/static/media/play.0832cabb.png"
+  },
+  {
+    "revision": "1d250b396861b67e4aa6335933960a4f",
+    "url": "/dnd-soundboard/static/media/reset.1d250b39.jpg"
+  }
+]);
