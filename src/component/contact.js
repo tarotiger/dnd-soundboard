c0c609381afe3e4b90b60b63bebf122225004d7a
Kenneth Lu <z5259931@cse.unsw.edu.au>
2020-06-03 19:37:37 +1000

    Changed port of contact.js

--- src/component/contact.js
+++ src/component/contact.js
@@ -49,7 +49,7 @@ export default class Contact extends React.Component {
 
         axios({
             method: "POST",
-            url: "http://ec2-3-21-55-21.us-east-2.compute.amazonaws.com:443/send",
+            url: "/send",
             data: this.state 
         })
         .then((response) => {
